41809b7705f61c3a022ad38cc3c0fba383822fdf
PUTUL <PUTUL@PUTUL-PC>
2016-02-09 08:55:05 -0800

    added an index file

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!--
+Developer: Sahana Parvin
+Email    : sahanaparvin99@yahoo.com
+-->
+<html>
+    <head>
+        <title>Practice</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    </head>
+    <body>
+        
+    </body>
+</html>
